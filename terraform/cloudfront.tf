08d657fe4886f71a74d808238aff314337ac0454
cristobal zuluaga <cristobalzuluagas@gmail.com>
2024-01-28 17:58:18 -0500

    changes in oac

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_cloudfront_origin_access_control" "default" {
   name                              = local.s3_origin_id
-  description                       = "Dev OAC Policy"
+  description                       = "${var.env} OAC Policy"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
